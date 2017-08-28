43eb8c9b32e15d1188e5efbc8c025a0837861d55
Randy Hofstede <randy.hofstede@outlook.com>
2017-08-28 06:39:29 +0200

    First Commit

---
+++ Ladders/Assets/Scrips/Ladder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+	private Player thePlayer;
+	private Enemy theEnemy;
+	
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = FindObjectOfType<Player>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "Player"){
+			thePlayer.onLadder = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.name == "Player"){
+			thePlayer.onLadder = false;
+		}
+	}
+
+}
